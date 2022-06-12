eac457840e54c04c361ef6e7bd3d0d4346a2304f
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-06-13 00:07:16 +0100

    update environment setup

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const app = express();
+const authRoutes = require(`./routes/authRoute`);
 
+// middlewares
+app.use(express.json());
+app.use(`/`, authRoutes);
+
+
+
+//IMPORT MONGOOSE-----------------------------------------------------------------
+const mongoose = require("mongoose"); //import mongoose
+const url = `mongodb://localhost:27017/userAuth`;
+// connect to mongoose
+mongoose.connect(url, function (err) { 
+  if (err) { console.log(err); }
+  console.log(`Connected to MongoDB`);
+});
+
+// -----------------------------------------------------------------
 
 
 app.listen(3000, () => {
---
+++ models/schema.js
@@ -0,0 +1,8 @@
+// const mongoose = require("mongoose");
+// const { Schema } = mongoose;
+
+
+
+
+// const MongooseModel = mongoose.model("u", mongooseSchema);
+// module.exports = MongooseModel;
